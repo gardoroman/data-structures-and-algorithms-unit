ff050b73ce581c883a83a08594c38f6382565c0f
gardoroman <gardoroman@gmail.com>
2016-12-03 17:51:54 -0600

    Code and test initial implemention of ArrayList

--- ds/array_list_spec.rb
+++ ds/array_list_spec.rb
@@ -16,18 +16,65 @@ describe ArrayList do
   end
 
   describe "#add" do
-    before(:each) do
-      @added_element = array_list.add("added element")
-    end
-    # let(:added_element) {array_list.add("added element")}
+
+    let(:added_element) {array_list.add("added element")}
 
     it "adds an element to end of ArrayList" do
-      expect(@added_element).to eq("added element")
+      expect(added_element).to eq("added element")
     end
 
     it "increases the size of the ArrayList" do
-      expect(array_list.length).to eq(6)
+      expect { array_list.add("another element") }.to change { array_list.length }.by(1)
     end
+  end
+
+  describe "#set" do
+    it "sets a value at the specified index" do
+      expect(array_list.set(0,1)).to eq(1)
+    end
+  end
+
+  describe "#get" do
+    it "returns the value of element at specified index" do
+      array_list.set(0,1)
+      expect(array_list.get(0)).to eq(1)
+    end
+  end
+
+  describe "#insert" do
+
+    let(:inserted_element) {array_list.insert(3, "inserted element")}
+
+    it "inserts an element at the desired index" do
+      expect(inserted_element).to eq("inserted element")
+    end
+
+    it "increases the size of the ArrayList" do
+      expect { array_list.insert(3, "insert another") }.to change { array_list.length }.by(1)
+    end
+  end
+
+  describe "OutOfBoundsException" do
+    it "raises error when index exceeds size" do
+      expect { array_list.set(6, "x")}.to raise_exception(OutOfBoundsException)
+    end
+
+    it "raises error when retrieving a non-existing index" do
+      expect { array_list.get(10)}.to raise_exception(OutOfBoundsException)
+    end
+
+    it "raises error when index less than Zero" do
+      expect { array_list.get(-3)}.to raise_exception(OutOfBoundsException)
+    end
+
+    it "raises and error when inserting at index less than Zero" do
+      expect { array_list.insert(-3, "too low")}.to raise_exception(OutOfBoundsException)
+    end
+
+    it "raises and error when inserting at index greater than length of ArrayList" do
+      expect { array_list.insert(30, "too big")}.to raise_exception(OutOfBoundsException)
+    end
+
   end
 
 end
