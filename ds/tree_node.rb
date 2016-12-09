12cecce14b66977d8663b30ae0b57fffd14835c9
gardoroman <gardoroman@gmail.com>
2016-12-09 16:21:39 -0600

    Create special implementation of Node for Tree class

---
+++ ds/tree_node.rb
@@ -0,0 +1,19 @@
+require_relative 'array_list'
+
+class TreeNode
+
+  attr_accessor :value, :children
+
+  def initialize(value)
+    @value = value
+    @children = ArrayList.new
+  end
+
+  def add_child(child)
+    @children.add(child)
+  end
+
+  def to_s
+    value
+  end
+end
